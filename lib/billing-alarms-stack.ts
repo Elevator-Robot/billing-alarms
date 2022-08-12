a2f4b5e31d7ab922335e8b1c4f6f0f1fb6be0968
Billy <aphexlog@gmail.com>
2022-08-12 16:58:32 +0000

    replacing aws-cdk-billing-alarm with my own construct, because I need to be able to ref the SNS topic arn

--- lib/billing-alarms-stack.ts
+++ lib/billing-alarms-stack.ts
@@ -1,16 +1,40 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { BillingAlarm } from 'aws-cdk-billing-alarm';
+// import { BillingAlarm } from 'aws-cdk-billing-alarm';
+import { Alarm, Metric } from 'aws-cdk-lib/aws-cloudwatch';
+import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
+import { Topic, Subscription, SubscriptionProtocol } from 'aws-cdk-lib/aws-sns';
 
 export class BillingAlarmsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // Create an alarm that emails `admin@example.com`
-    // if estimated charges exceed 50 USD
-    new BillingAlarm(this, 'AWSAccountBillingAlarm', {
-      monthlyThreshold: 50,
-      emails: ['aphexlog@gmail.com']
+    
+    const alarm = new Alarm(this, 'AWSAccountBillingAlarm', {
+      metric: new Metric({
+        namespace: 'AWS/Billing',
+        metricName: 'EstimatedCharges',
+        statistic: 'Maximum',
+        dimensionsMap: {
+          'Currency': 'USD'
+        }
+      }),
+      threshold: 50,
+      evaluationPeriods: 1,
+      actionsEnabled: true
     });
+
+    const topic = new Topic(this, 'BillingTopic', {
+      topicName: 'BillingTopic'
+    });
+
+    new Subscription(this, 'BillingSubscription', {
+      topic: topic,
+      protocol: SubscriptionProtocol.EMAIL,
+      endpoint: 'aphexlog@gmail.com'
+  });
+
+  new SnsAction(topic)
+
+
   }
 }
