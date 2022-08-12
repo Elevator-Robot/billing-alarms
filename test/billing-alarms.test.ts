3994ee5a8c50c742381cadc5e01ece2c2907f3bc
Billy <aphexlog@gmail.com>
2022-08-12 15:21:51 +0000

    staging cdk init=

---
+++ test/billing-alarms.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BillingAlarms from '../lib/billing-alarms-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/billing-alarms-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BillingAlarms.BillingAlarmsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
